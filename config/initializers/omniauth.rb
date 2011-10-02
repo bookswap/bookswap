0dc9f03eb2cf29974939d2c1c70cc23db0230372
Sid Shanker <hackny.bookswap@gmail.com>
2011-10-01 22:30:40 -0400

    Fixing the facebook auth stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '176683522412651', ' 251529384284f3ed476718174520e5be',  {:scope => "publish_stream, user_education_history, user_likes, friends_likes, email, offline_access, manage_pages"}
+  provider :facebook, '176683522412651', '251529384284f3ed476718174520e5be',  {:scope => "publish_stream, user_education_history, user_likes, friends_likes, email, offline_access, manage_pages"}
 end
