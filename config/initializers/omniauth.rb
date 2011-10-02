16db2313970ad1dd1d696988e73ee2db7374144c
Sid Shanker <hackny.bookswap@gmail.com>
2011-10-01 21:25:30 -0400

    Fixing heroku issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '172660092815473', '8fbb3f7f863299399eaf60a0d222eab1',  {:scope => "publish_stream, user_education_history, user_likes, friends_likes, email, offline_access, manage_pages", :client_options => {:ssl => {:verify => false}}}
+  provider :facebook, ' 176683522412651', ' 251529384284f3ed476718174520e5be',  {:scope => "publish_stream, user_education_history, user_likes, friends_likes, email, offline_access, manage_pages", :client_options => {:ssl => {:verify => false}}}
 end
